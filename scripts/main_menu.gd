0ad92adc499be33589d4fbb2214ba69db6d3f69e
dstr1989 <99421580+dstr1989@users.noreply.github.com>
2026-06-30 08:12:37 +0200

    Add de_oasis map: AI background art + invisible collision layout (sites, water, mid choke, cars as vault cover, raised site plateaus + ramps, sunken left tunnel); add to menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _on_oasis_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MapOasis.tscn")
+
 func _on_burrow_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
